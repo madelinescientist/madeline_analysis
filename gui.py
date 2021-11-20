a4d4b4b4411ecdc78a28a053d926d573c45c5573
NKeil <nicolaskeil.school@gmail.com>
2021-11-20 11:29:30 -0800

    rename input_folder to root_input_folder and remove output_file

--- gui.py
+++ gui.py
@@ -1,16 +1,15 @@
 import PySimpleGUI as psg
 
 
-def run_gui(input_folder, output_folder, output_file, fr):
+def run_gui(root_input_folder, output_folder, fr):
     # theme
     psg.theme('SystemDefault')
 
     # components inside window
-    layout = [[psg.Text('Default input folder: ' + input_folder, font=(None, 8))],
+    layout = [[psg.Text('Default input folder: ' + root_input_folder, font=(None, 8))],
               [psg.Text('Default output folder: ' + output_folder, font=(None, 8))],
-              [psg.Text('Input Folder:', size=(15, None)), psg.FolderBrowse(key='input_folder', initial_folder=input_folder)],
+              [psg.Text('Input Folder:', size=(15, None)), psg.FolderBrowse(key='input_folder', initial_folder=root_input_folder)],
               [psg.Text('Output Folder:', size=(15, None)), psg.FolderBrowse(key='output_folder', initial_folder=output_folder)],
-              [psg.Text('Output File Name:', size=(15, None)), psg.InputText(key='output_file', default_text=output_file)],
               [psg.Text('Frame Rate:', size=(15, None)), psg.InputText(key='fr', default_text=fr)],
               [psg.Button('Enter'), psg.Button('Cancel')]]
 
@@ -31,9 +30,8 @@ def run_gui(input_folder, output_folder, output_file, fr):
             print('Error: framerate should be an integer')
         else:
             fr = int(values['fr'])
-            output_file = values['output_file']
             if values['input_folder'] != '':
-                input_folder = values['input_folder']
+                root_input_folder = values['input_folder']
             if values['output_folder'] != '':
                 output_folder = values['output_folder']
 
@@ -41,8 +39,8 @@ def run_gui(input_folder, output_folder, output_file, fr):
     if window_closed:
         print('operation canceled.')
         return None
-    if input_folder == '' or output_folder == '' or output_file == '':
+    if root_input_folder == '' or output_folder == '':
         print('invalid input.')
         return None
-    print('gui.py: returning', input_folder, output_folder, output_file, fr)
-    return input_folder, output_folder, output_file, fr
+    print('gui.py: returning', root_input_folder, output_folder, fr)
+    return root_input_folder, output_folder, fr
